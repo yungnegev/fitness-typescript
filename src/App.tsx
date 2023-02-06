0283e1ab0be0e8fde8e7e2d83f3eeec55f3a9b95
yungnegev <neiklotrr@gmail.com>
2023-02-06 18:15:52 +0300

    navbar implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import Navbar from "@/scenes/navbar"
+import { useState } from "react"
+
 
 function App() {
 
+  const [selectedPage, setSelectedPage] = useState<string>('home')
+
   return (
-    <div className="app">
-      
+    <div className="app bg-gray-20">
+      <Navbar 
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
     </div>
   )
 }
