817800b5f1b6ac4c9938573e2ff1b26dac95e632
yungnegev <neiklotrr@gmail.com>
2023-02-13 16:34:53 +0300

    Classes page

---
+++ src/scenes/ourClasses/index.tsx
@@ -0,0 +1,96 @@
+import { SelectedPage, ClassType } from '@/shared/types'
+import { motion as m } from 'framer-motion'
+import image1 from '@/assets/image1.png'
+import image2 from '@/assets/image2.png'
+import image3 from '@/assets/image3.png'
+import image4 from '@/assets/image4.png'
+import image5 from '@/assets/image5.png'
+import image6 from '@/assets/image6.png'
+import HText from '@/shared/HText'
+import Class from './Class'
+
+const classes: Array<ClassType> = [
+    {
+        name: "Weight Training Classes",
+        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis perferendis sapiente nesciunt, maiores repellat dolorum sint veritatis expedita esse illum!",
+        image: image1,
+
+    },
+    {
+        name: "Yoga Classes",
+        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis perferendis sapiente nesciunt, maiores repellat dolorum sint veritatis expedita esse illum!",
+        image: image2,
+
+    },
+    {
+        name: "Aerobic Classes",
+        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis perferendis sapiente nesciunt, maiores repellat dolorum sint veritatis expedita esse illum!",
+        image: image3,
+
+    },
+    {
+        name: "Special Coaching",
+        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis perferendis sapiente nesciunt, maiores repellat dolorum sint veritatis expedita esse illum!",
+        image: image4,
+
+    },
+    {
+        name: "Diet and Suplement Training",
+        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis perferendis sapiente nesciunt, maiores repellat dolorum sint veritatis expedita esse illum!",
+        image: image5,
+
+    },
+    {
+        name: "Strength Training Classes",
+        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis perferendis sapiente nesciunt, maiores repellat dolorum sint veritatis expedita esse illum!",
+        image: image6,
+
+    }
+]
+
+type Props = {
+    setSelectedPage: (value: SelectedPage) => void
+}
+
+const ourClasses = ({ setSelectedPage }: Props) => {
+  return (
+    <section className='w-full bg-primary-100 py-40' id='ourclasses'>
+        <m.div 
+          onViewportEnter={() => setSelectedPage(SelectedPage.OurClasses)}
+          >
+            <m.div
+              className='mx-auto w-5/6'
+              initial="hidden"
+              whileInView="vidible"
+              viewport={{ once: true, amount: 0.5 }}
+              transition={{ duration: 0.5 }}
+              variants={{
+                  hidden: { opacity: 0, x: -70 },
+                  vidible: { opacity: 1, x: 0 },
+              }}
+              >
+                <div className='md:w-3/5'>
+                    <HText>OUR CLASSES</HText>
+                    <p className='py-5'>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio aliquid odio voluptatibus placeat ratione at quod quam omnis neque, magni repellat obcaecati quibusdam ipsa dicta. Aut temporibus suscipit repellat atque dignissimos quidem eos, nobis harum.
+                    </p>
+                </div>
+            </m.div>
+            <div className='mt-10 h-[353px] w-full overflow-x-auto overflow-y-hidden'>
+              <ul className='w-[2800px] whitespace-nowrap'>
+                {classes.map((item: ClassType, index) => (
+                    <Class 
+                      key={`${item.name}-${index}`}
+                      name={item.name}
+                      description={item.description}
+                      image={item.image}
+                      />
+                ))}
+              </ul>
+            </div>
+        </m.div>
+    </section>
+  )
+}
+
+export default ourClasses
