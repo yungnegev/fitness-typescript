ca61536be099dddf8800775f8d065a732752318e
yungnegev <neiklotrr@gmail.com>
2023-02-13 15:41:12 +0300

    benefits page complete

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -3,6 +3,8 @@ import Benefit from "./Benefit"
 import { SelectedPage, BenefitType } from "@/shared/types"
 import { HomeModernIcon, UserGroupIcon, AcademicCapIcon } from "@heroicons/react/24/solid"
 import { motion as m } from "framer-motion"
+import ActionButton from "@/shared/ActionButton"
+import BenefitsPageGraphic from "@/assets/BenefitsPageGraphic.png"
 
 const benefits: Array<BenefitType> = [
   {
@@ -41,12 +43,22 @@ const Benefits = ({ setSelectedPage }: Props) => {
         onViewportEnter={() => setSelectedPage(SelectedPage.Benefits)}
         >
           {/* HEADER */}
-          <div className="md:my-5 md:w-3/5">
+          <m.div 
+            className="md:my-5 md:w-3/5"
+            initial="hidden"
+            whileInView="vidible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ duration: 0.5 }}
+            variants={{
+                hidden: { opacity: 0, x: -70 },
+                vidible: { opacity: 1, x: 0 },
+            }}
+            >
             <HText>MORE THAN JUST A GYM.</HText>
             <p className="my-5 text-sm">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet rerum quo fuga, molestiae dicta est ducimus atque, quam, iure excepturi tempora temporibus qui quibusdam eligendi sint cumque deleniti iste officia praesentium. Inventore earum error voluptate temporibus distinctio amet, consectetur dicta.
             </p>
-          </div>
+          </m.div>
           {/* BENEFITS */}
           <m.div 
             className="md:flex items-center justify-between gap-8 mt-5"
@@ -65,6 +77,67 @@ const Benefits = ({ setSelectedPage }: Props) => {
                 />
             ))}
           </m.div>
+          {/* GRAPHICS AND DESCRIPTION */}
+          <div className="mt-16 items-center justify-between gap-20 md:mt-28 md:flex">
+            {/* GRAPHIC */}
+            <img
+              className="mx-auto" 
+              src={BenefitsPageGraphic} 
+              alt="benefitsimage" 
+              />
+            {/* DESCRIPTION */}
+            <div>
+              {/* TITLE */}
+              <div className="relative">
+                <div className="before:absolute before:-top-20 before:-left-20 before:z-[-1] before:content-abstractwaves">
+                  <m.div
+                      className="md:my-5 md:w-3/5"
+                      initial="hidden"
+                      whileInView="vidible"
+                      viewport={{ once: true, amount: 0.5 }}
+                      transition={{ duration: 0.5 }}
+                      variants={{
+                          hidden: { opacity: 0, x: -70 },
+                          vidible: { opacity: 1, x: 0 },
+                      }}
+                    >
+                    <HText>
+                      MILLIONS OF HAPPY MEMBERS GETTING {" "}
+                      <span className="text-primary-500"> FIT </span>
+                    </HText>
+                  </m.div>                  
+                </div>
+              </div>
+
+              {/* DESCRIPTION */}
+                <m.div
+                  className="md:my-5 md:w-3/5"
+                  initial="hidden"
+                  whileInView="vidible"
+                  viewport={{ once: true, amount: 0.5 }}
+                  transition={{ delay: 0.2, duration: 0.5 }}
+                  variants={{
+                      hidden: { opacity: 0, x: -70 },
+                      vidible: { opacity: 1, x: 0 },
+                  }}
+                >
+                  <p className="my-5">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus nisi, quos omnis quo modi quia odio quibusdam sint atque provident est repudiandae facilis, officia necessitatibus fugiat neque quisquam velit quidem!
+                  </p>
+                  <p className="mb-5">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi reiciendis fugiat natus amet eius eaque dolorum sit eos nulla harum!
+                  </p>
+                </m.div>
+              {/* BUTTON */}
+              <div className="relative mt-16">
+                <div className="before:absolute before:-bottom-20 before:right-40 before:z-[-1] before:content-sparkles">
+                  <ActionButton setSelectedPage={setSelectedPage}>
+                    Join Now
+                  </ActionButton>
+                </div>
+              </div>
+            </div>
+          </div>
       </m.div>
     </section>
   )
