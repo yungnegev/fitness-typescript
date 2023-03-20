52f9e876175f87cfa60c975e4930d7642fbb4560
Arthur <arthur@monad.local>
2023-03-20 21:58:43 +0300

    commit

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -6,12 +6,12 @@ import { SelectedPage } from "@/shared/types"
 import useMediaQuery from "@/hooks/useMediaQuery"
 import ActionButton from "@/shared/ActionButton"
 
-
 type Props = {
   isTopOfPage: boolean
   selectedPage: SelectedPage
   setSelectedPage: (value: SelectedPage) => void
 }
+
 
 const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }: Props) => {
 
@@ -29,42 +29,42 @@ const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }: Props) => {
           <div className={`${flexBetween} w-full gap-16`}>
             <img src={Logo} alt="logo" />
             {isAboveMediumScreens ? (
-            <div className={`${flexBetween} w-full`}>
-              <div className={`${flexBetween} gap-8 text-sm`}>
-                <Link 
-                  page="Home" 
-                  selectedPage={selectedPage} 
-                  setSelectedPage={setSelectedPage}
-                />
-                <Link 
-                  page="Benefits"
-                  selectedPage={selectedPage} 
-                  setSelectedPage={setSelectedPage}
-                />
-                <Link 
-                  page="Our Classes"
-                  selectedPage={selectedPage} 
-                  setSelectedPage={setSelectedPage} 
-                />
-                <Link 
-                  page="Contact Us"
-                  selectedPage={selectedPage} 
-                  setSelectedPage={setSelectedPage} 
-                />
-              </div>
-              <div className={`${flexBetween} gap-8`}>
-                <p>Sign In</p>
-                <ActionButton setSelectedPage={setSelectedPage}>Become a Member</ActionButton>
-              </div>
-            </div> )
-            : (
-              <button
-                className="rounded-full bg-secondary-500 p-2"
-                onClick={() => setIsMenuToggled(!isMenuToggled)}
+              <div className={`${flexBetween} w-full`}>
+                <div className={`${flexBetween} gap-8 text-sm`}>
+                  <Link
+                    page="Home"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                  <Link
+                    page="Benefits"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                  <Link
+                    page="Our Classes"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                  <Link
+                    page="Contact Us"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                </div>
+                <div className={`${flexBetween} gap-8`}>
+                  <p>Sign In</p>
+                  <ActionButton setSelectedPage={setSelectedPage}>Become a Member</ActionButton>
+                </div>
+              </div>)
+              : (
+                <button
+                  className="rounded-full bg-secondary-500 p-2"
+                  onClick={() => setIsMenuToggled(!isMenuToggled)}
                 >
-                  <Bars3Icon className="h-6 w-6 text-white"/>
-              </button>
-            )}
+                  <Bars3Icon className="h-6 w-6 text-white" />
+                </button>
+              )}
           </div>
         </div>
       </div>
@@ -75,30 +75,30 @@ const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }: Props) => {
           {/* CLOSE ICON */}
           <div className="flex justify-end p-12">
             <button onClick={() => setIsMenuToggled(!isMenuToggled)}>
-              <XMarkIcon className="h-6 w-6 text-gray-400"/>
+              <XMarkIcon className="h-6 w-6 text-gray-400" />
             </button>
           </div>
           <div className="ml-[33%] flex flex-col gap-10 text-2xl">
-                <Link 
-                  page="Home" 
-                  selectedPage={selectedPage} 
-                  setSelectedPage={setSelectedPage}
-                />
-                <Link 
-                  page="Benefits"
-                  selectedPage={selectedPage} 
-                  setSelectedPage={setSelectedPage}
-                />
-                <Link 
-                  page="Our Classes"
-                  selectedPage={selectedPage} 
-                  setSelectedPage={setSelectedPage} 
-                />
-                <Link 
-                  page="Contact Us"
-                  selectedPage={selectedPage} 
-                  setSelectedPage={setSelectedPage} 
-                />
+            <Link
+              page="Home"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Benefits"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Our Classes"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Contact Us"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
           </div>
         </div>
       )}
